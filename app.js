6245905a41eb47a90b05921b2fe266e04901dedb
Coli-co <awestlifeblue@gmail.com>
2022-11-03 08:58:23 +0800

    Set products page

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
-// app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/products', (req, res) => {
+  res.render('products')
 })
 
 app.listen(port, () => {
