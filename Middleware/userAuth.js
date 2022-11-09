f361ffab3b843cc4fe2857cc4692f4be90a5888b
Coli-co <awestlifeblue@gmail.com>
2022-11-09 19:38:13 +0800

    Create user authentication middleware

---
+++ Middleware/userAuth.js
@@ -0,0 +1,37 @@
+// Check for duplicate usernames and emails
+const express = require('express')
+const db = require('../models/index')
+
+const User = db.users
+
+const saveUser = async (req, res, next) => {
+  try {
+    // check duplicate username
+    const username = await User.findOne({
+      where: {
+        userName: req.body.userName
+      }
+    })
+
+    if (username) {
+      return res.json(409).send('Username had already been used!')
+    }
+
+    // check duplicate email
+    const email = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (email) {
+      return res.json(409).send('Email had already been used!')
+    }
+
+    next()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+module.exports = { saveUser }
